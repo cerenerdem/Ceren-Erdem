4b3389eabe5328e378354ad478a3b6d1fee520d6
cerenerdem <cerennerdem06@gmail.com>
2024-11-04 01:57:44 +0300

    Add files via upload

---
+++ Ceren-ERDEM.py
@@ -0,0 +1,8 @@
+x = 9
+y = 29
+total = x + y
+print("total:", total) 
+name = "Adriana"
+surname = "Lima"
+full_name = name + " " + surname  # String merge
+print("Full Name:", full_name)
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Ceren-Erdem
-deneme1-2-3
+# deneme2
